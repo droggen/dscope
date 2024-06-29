626631004bc9d71b8a22c2127c985ecdec5c2944
droggen <droggen@gmail.com>
2024-06-29 12:21:42 +0100

    main: fix style on Android - do not use native style as broken with widget on material.

--- source/main.cpp
+++ source/main.cpp
@@ -24,7 +24,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 int main(int argc, char *argv[])
 {
-    qputenv("QT_USE_ANDROID_NATIVE_STYLE", "1");
+    qputenv("QT_USE_ANDROID_NATIVE_STYLE", "0");
 
     QApplication a(argc, argv);
     MainWindow w;
